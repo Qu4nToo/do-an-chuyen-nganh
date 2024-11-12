753b909ce068cb1f8e5b243646581c03fb0b4fac
Trần Nguyễn Hoàng Quân <tnhq1210@gmail.com>
2024-11-12 19:50:23 +0700

    push menu

--- components/features/best-seller.tsx
+++ components/features/best-seller.tsx
@@ -12,49 +12,49 @@ import ProductCard from "../ui/product-card";
 export default function BestSeller() {
   return (
     <div className="w-full h-full pt-10 pb-10 px-10">
-      <div className="m-0 flex items-start w-full p-auto lg:px-14">
-        <p className="max-w-70xl pl-4 text-xl md:text-5xl font-bold text-red-700 dark:text-neutral-200 font-sans">
-          BestSeller
-        </p>
-      </div>
-      <div className="mx-auto p-5 gap-5 items-center p-auto w-[80%] md:w-full">
-        <Carousel>
-          <CarouselContent className="p-11">
-            <CarouselItem className="md:basis-1/2 lg:basis-1/3 flex justify-center">
-              <ProductCard />
-            </CarouselItem>
-            <CarouselItem className="md:basis-1/2 lg:basis-1/3 flex justify-center">
-              <ProductCard />
-            </CarouselItem>
-            <CarouselItem className="md:basis-1/2 lg:basis-1/3 flex justify-center">
-              <ProductCard />
-            </CarouselItem>
-            <CarouselItem className="md:basis-1/2 lg:basis-1/3 flex justify-center">
-              <ProductCard />
-            </CarouselItem>
-            <CarouselItem className="md:basis-1/2 lg:basis-1/3 flex justify-center">
-              <ProductCard />
-            </CarouselItem>
-            <CarouselItem className="md:basis-1/2 lg:basis-1/3 flex justify-center">
-              <ProductCard />
-            </CarouselItem>
-            <CarouselItem className="md:basis-1/2 lg:basis-1/3 flex justify-center">
-              <ProductCard />
-            </CarouselItem>
-            <CarouselItem className="md:basis-1/2 lg:basis-1/3 flex justify-center">
-              <ProductCard />
-            </CarouselItem>
-            <CarouselItem className="md:basis-1/2 lg:basis-1/3 flex justify-center">
-              <ProductCard />
-            </CarouselItem>
-          </CarouselContent>
-          <div className="mt-4 container flex justify-center">
-            <CarouselPrevious />
-            <p>&ensp;</p>
-            <CarouselNext />
-          </div>
-        </Carousel>
-      </div>
+    <div className="m-0 flex items-start w-full p-auto lg:px-14">
+      <p className="max-w-70xl pl-4 text-xl md:text-5xl font-bold text-red-700 dark:text-neutral-200 font-sans">
+        BestSeller
+      </p>
     </div>
+    <div className="mx-auto p-0 md:p-5 gap-5 items-center p-auto w-auto">
+      <Carousel>
+        <CarouselContent className="md:p-11">
+          <CarouselItem className="sm:basis-full md:basis-1/2 lg:basis-1/3 flex justify-center">
+            <ProductCard />
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3 flex justify-center">
+            <ProductCard />
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3 flex justify-center">
+            <ProductCard />
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3 flex justify-center">
+            <ProductCard />
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3 flex justify-center">
+            <ProductCard />
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3 flex justify-center">
+            <ProductCard />
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3 flex justify-center">
+            <ProductCard />
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3 flex justify-center">
+            <ProductCard />
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3 flex justify-center">
+            <ProductCard />
+          </CarouselItem>
+        </CarouselContent>
+        <div className="mt-4 container flex justify-center">
+          <CarouselPrevious />
+          <p>&ensp;</p>
+          <CarouselNext />
+        </div>
+      </Carousel>
+    </div>
+  </div>
   );
 }
--- components/features/navigation.tsx
+++ components/features/navigation.tsx
@@ -11,11 +11,13 @@ import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
-function classNames(...classes) {
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+
+function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 const navigation = [
-  { name: "Home", href: "#", current: true },
+  { name: "Home", href: "/", current: false },
   { name: "About", href: "/about", current: false },
   { name: "Menu", href: "/menu", current: false },
   { name: "Promotion", href: "/promotion", current: false },
@@ -25,7 +27,7 @@ export default function Navbar() {
   return (
     <Disclosure
       as="nav"
-      className="bg-white border-solid border-b-2 border-black h-auto"
+      className="bg-white border-solid border-b-2 border-black h-auto sticky top-0 z-10"
     >
       <div className="container max-w-full px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-auto">
@@ -54,7 +56,7 @@ export default function Navbar() {
                 />
               </Link>
             </div>
-            <div className="hidden sm:ml-6  sm:block md:flex md:items-center">
+            <div className="hidden mx-auto  sm:block md:flex md:items-center">
               <div className="flex space-x-4 w-auto">
                 {navigation.map((item) => (
                   <Link
@@ -71,117 +73,68 @@ export default function Navbar() {
                     {item.name}
                   </Link>
                 ))}
-                <button className="my-1 ml-10 flex h-8 w-8 items-center justify-center rounded-lg lg:ml-8 md:flex lg:hidden">
-                  <span className="sr-only">Search components</span>
-                  <svg
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 fill-gray-900 hover:fill-gray-900"
-                  >
-                    <path d="M20.47 21.53a.75.75 0 1 0 1.06-1.06l-1.06 1.06Zm-9.97-4.28a6.75 6.75 0 0 1-6.75-6.75h-1.5a8.25 8.25 0 0 0 8.25 8.25v-1.5ZM3.75 10.5a6.75 6.75 0 0 1 6.75-6.75v-1.5a8.25 8.25 0 0 0-8.25 8.25h1.5Zm6.75-6.75a6.75 6.75 0 0 1 6.75 6.75h1.5a8.25 8.25 0 0 0-8.25-8.25v1.5Zm11.03 16.72-5.196-5.197-1.061 1.06 5.197 5.197 1.06-1.06Zm-4.28-9.97c0 1.864-.755 3.55-1.977 4.773l1.06 1.06A8.226 8.226 0 0 0 18.75 10.5h-1.5Zm-1.977 4.773A6.727 6.727 0 0 1 10.5 17.25v1.5a8.226 8.226 0 0 0 5.834-2.416l-1.061-1.061Z"></path>
-                  </svg>
-                </button>
               </div>
-            </div>
-            <div className="mx-auto px-4 py-2 rounded-md border-2 border-black h-10 overflow-hidden max-w-lg w-auto font-[sans-serif] hidden md:hidden lg:flex">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 192.904 192.904"
-                width="16px"
-                className="fill-black mr-3 rotate-90"
-              >
-                <path d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z"></path>
-              </svg>
-              <input
-                type="email"
-                placeholder="Tìm kiếm..."
-                className="w-full outline-none bg-transparent text-gray-600 text-sm"
-              />
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center gap-2 pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button
-              type="button"
-              className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-            >
-              <span className="absolute -inset-1.5" />
-              <span className="sr-only">View notifications</span>
-              <BellIcon aria-hidden="true" className="h-6 w-6" />
-            </button>
-            <Link href="/Login">
+            <Link href="Cart">
               <button
                 type="button"
                 className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
               >
                 <span className="absolute -inset-1.5" />
-                <span className="sr-only">Login</span>
-                <FontAwesomeIcon icon={faUser} className="h-5 w-6" />
+                <span className="sr-only">Shopping Cart</span>
+                <FontAwesomeIcon icon={faCartShopping} className="h5 w-6" />
               </button>
             </Link>
-
             {/* Profile dropdown */}
-            {/* <Menu as="div" className="relative ml-3">
-                <div>
-                  <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                    <span className="absolute -inset-1.5" />
-                    <span className="sr-only">Open user menu</span>
-                    <img
+            <Menu as="div" className="relative ml-3">
+              <div>
+                <MenuButton className="relative flex rounded-full bg-gray-800 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">Open user menu</span>
+                  <img
                       alt=""
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      src="/user.png"
                       className="h-8 w-8 rounded-full"
                     />
-                  </MenuButton>
-                </div>
-                <MenuItems
-                  transition
-                  className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-                >
-                  <MenuItem>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
-                    >
-                      Your Profile
-                    </a>
-                  </MenuItem>
-                  <MenuItem>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
-                    >
-                      Settings
-                    </a>
-                  </MenuItem>
-                  <MenuItem>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
-                    >
-                      Sign out
-                    </a>
-                  </MenuItem>
-                </MenuItems>
-              </Menu> */}
+                </MenuButton>
+              </div>
+              <MenuItems
+                transition
+                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+              >
+                <MenuItem>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                  >
+                    Your Profile
+                  </a>
+                </MenuItem>
+                <MenuItem>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                  >
+                    Settings
+                  </a>
+                </MenuItem>
+                <MenuItem>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                  >
+                    Sign out
+                  </a>
+                </MenuItem>
+              </MenuItems>
+            </Menu>
           </div>
         </div>
       </div>
       <DisclosurePanel className="sm:hidden">
         <div className="space-y-1 px-2 pb-3 pt-2">
-          <div className="px-4 py-2 rounded-md border-2 border-black h-10 overflow-hidden font-[sans-serif] flex w-full">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 192.904 192.904"
-              width="16px"
-              className="fill-black mr-3 rotate-90"
-            >
-              <path d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z"></path>
-            </svg>
-            <input
-              type="email"
-              placeholder="Tìm kiếm..."
-              className="w-full outline-none bg-transparent text-gray-600 text-sm"
-            />
-          </div>
           {navigation.map((item) => (
             <DisclosureButton
               key={item.name}
